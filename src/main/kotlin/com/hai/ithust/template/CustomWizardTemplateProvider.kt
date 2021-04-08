4652e6f870252b7ca0ad70487953f3df2d8da827
CongHai <conghai.luong@foodycorp.vn>
2021-04-08 13:33:44 +0700

    [Init] init source

--- src/main/kotlin/com/hai/ithust/template/CustomWizardTemplateProvider.kt
+++ src/main/kotlin/com/hai/ithust/template/CustomWizardTemplateProvider.kt
@@ -1,9 +1,8 @@
 package com.hai.ithust.template
 
+import com.android.tools.idea.wizard.template.Template
 import com.android.tools.idea.wizard.template.WizardTemplateProvider
-import com.hai.ithust.template.fragment.HiltViewModelFragmentTemplate
 
 class CustomWizardTemplateProvider : WizardTemplateProvider() {
-    override fun getTemplates() =
-            listOf(HiltViewModelFragmentTemplate)
+    override fun getTemplates() = emptyList<Template>()
 }
